36c45321dc64645119fdbc03bb7cba2bab75aa63
Tracie Masek <traciemasek@gmail.com>
2018-11-13 23:29:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  [...array, "element"];
+  array = [...array, "element"];
   return array
 }
 
