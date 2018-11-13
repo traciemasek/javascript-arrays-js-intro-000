9f6327397a72ba35b92bd7ddb53881de1260fd7d
Tracie Masek <traciemasek@gmail.com>
2018-11-13 23:34:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,17 +1,17 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array];
+  return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element);
+  return array.unshift(element)
 }
 
 function addElementToEndOfArray(array, element) {
-  return [...array, element];
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  return array.push(element);
+  return array.push(element)
 }
