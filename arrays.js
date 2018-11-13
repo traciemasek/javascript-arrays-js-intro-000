1be506951d2d6924c8372cebc90ce06eaf73109d
Tracie Masek <traciemasek@gmail.com>
2018-11-13 23:31:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,5 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift("element");
-  return array
+  return array.unshift("element");
 }
